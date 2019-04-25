f1ba940950795a8bebd4fd31595194fd76234b93
Eric Durante <eric@westarete.com>
2019-04-25 17:28:38 -0400

    WIP integrating new HTML/CSS design with existing javascript

--- js/profile.js
+++ js/profile.js
@@ -39,7 +39,57 @@ $("document").ready(function() {
     var template = Handlebars.compile(source);
     var rendered_html = template(context);
     $('#profile').append(rendered_html);
-    $('#tabs').tabs();
+
+    $(document).foundation();
+
+    // Scroll
+    $('.tabs li a').on('click', function() {
+      $([document.documentElement, document.body]).animate({
+          scrollTop: $(".tabs-wrapper").offset().top + 20
+      }, 150);
+    });
+
+    // Sticky nav
+    var tabNav = $('#profile-tabs');
+    var stickyNavTop = tabNav.offset().top;
+    var navWidth = tabNav.width();
+    var padding = tabNav.height();
+
+    var stickyNav = function(){
+      var scrollTop = $(window).scrollTop(); 
+      if (scrollTop > stickyNavTop) { 
+        tabNav.addClass('sticky');
+        $('.tabs-wrapper').css('padding-top', padding);
+        if (Foundation.MediaQuery.is('large')) {
+          tabNav.css('width', navWidth);
+        }
+      } else {
+        tabNav.removeClass('sticky'); 
+        tabNav.css('width', '100%');
+        $('.tabs-wrapper').css('padding-top', 0);
+      }
+    };
+
+    stickyNav();
+
+    $(window).scroll(function() {
+      stickyNav();
+    });
+
+    // Move upper nav to hamburger container
+    var moveNav = function(){
+      if (Foundation.MediaQuery.atLeast('large')) {
+        $('#upper-nav').appendTo("#upper-nav-wrapper");
+      } else {
+        $('#upper-nav').appendTo("#lower-nav");
+      }
+    };
+
+    moveNav();
+
+    $(window).resize(function() {
+      moveNav();
+    });
   }
 
   $.ajax(profile_api_url, {
